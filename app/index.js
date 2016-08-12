aedd7ce3e871bad2990b2418796d0b1bd69ff8b8
Simon Heys <si@simonheys.com>
2016-08-12 11:22:03 +0100

    Spawn a child process and read output via stdout

--- app/index.js
+++ app/index.js
@@ -7,6 +7,20 @@ import routes from './routes';
 import configureStore from './store/configureStore';
 import './app.global.css';
 
+const platform = require('os').platform();
+console.log('platform', platform);
+
+console.log('process.cwd():', process.cwd());
+
+const spawn = require('child_process').spawn;
+
+const child = spawn('ls', ['-la']);
+child.stdout.on("data", (data) => {
+  const dataString = data.toString('utf8');
+  console.log(dataString);
+  // console.log('data', data);
+});
+
 const store = configureStore();
 const history = syncHistoryWithStore(hashHistory, store);
 
