1e75eaf56194004c81217b1eb69f38639dc0e059
Simon Heys <si@simonheys.com>
2016-09-01 07:57:47 +0100

    WIP metadata form

--- app/components/metadata/Metadata.js
+++ app/components/metadata/Metadata.js
@@ -2,12 +2,45 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import styles from './Metadata.css';
 
+const locations = require('static/locations.json');
+
 class Metadata extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: 'GB'
+    };
+  }
+
+  handleChange(event) {
+    console.log('handleChange', event);
+    var state = {};
+    state[event.target.name] = event.target.value;
+    console.log('updating state', state);
+    this.setState(state);
+  }
+
+  locationSelect() {
+    var options = [];
+    locations.forEach((location, index) => {
+      options.push(
+        <option key={index} value={location['alpha-2']}>{location.name}</option>
+      );
+    });
+
+    return (
+      <select name="location" value={this.state.location} onChange={(event) => this.handleChange(event)}>
+        {options}
+      </select>
+    );
+  }
+
   render() {
     const {children} = this.props;
     return (
       <div className={styles.container}>
-        Metadata
+        {this.locationSelect()}
+        {children}
       </div>
     );
   }
