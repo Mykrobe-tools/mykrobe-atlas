160e49c0023deae9e79e2c41c10eb04bb71d6183
Simon Heys <si@simonheys.com>
2016-09-01 09:35:12 +0100

    Form layout

--- app/components/metadata/Metadata.js
+++ app/components/metadata/Metadata.js
@@ -74,31 +74,40 @@ class Metadata extends Component {
             {drug}
           </td>
           <td>
-            <input
-              type="radio"
-              name={drug}
-              value="S"
-              defaultChecked={'S' === this.state.susceptibility[drug]}
-              onChange={(event) => this.handleChange(event)}
-            />
+            <label>
+              <input
+                type="radio"
+                name={drug}
+                value="S"
+                defaultChecked={'S' === this.state.susceptibility[drug]}
+                onChange={(event) => this.handleChange(event)}
+              />
+              Susceptible
+            </label>
           </td>
           <td>
-            <input
-              type="radio"
-              name={drug}
-              value="R"
-              defaultChecked={'R' === this.state.susceptibility[drug]}
-              onChange={(event) => this.handleChange(event)}
-            />
+            <label>
+              <input
+                type="radio"
+                name={drug}
+                value="R"
+                defaultChecked={'R' === this.state.susceptibility[drug]}
+                onChange={(event) => this.handleChange(event)}
+              />
+              Resistant
+            </label>
           </td>
           <td>
-            <input
-              type="radio"
-              name={drug}
-              value="I"
-              defaultChecked={'I' === this.state.susceptibility[drug]}
-              onChange={(event) => this.handleChange(event)}
-            />
+            <label>
+              <input
+                type="radio"
+                name={drug}
+                value="I"
+                defaultChecked={'I' === this.state.susceptibility[drug]}
+                onChange={(event) => this.handleChange(event)}
+              />
+              Inconclusive
+            </label>
           </td>
         </tr>
       );
@@ -116,41 +125,48 @@ class Metadata extends Component {
     const {children} = this.props;
     return (
       <div className={styles.container}>
-        <div className={styles.formRow}>
-          <label htmlFor="location">Collection location</label>
-          {this.locationSelect()}
+        <div className={styles.formContainer}>
+          <div className={styles.formRow}>
+            <label htmlFor="location">Collection location</label>
+            {this.locationSelect()}
+          </div>
+          <div className={styles.formRow}>
+            <label htmlFor="location">Collection date</label>
+            {/* DatePicker currently shows US locale since Electron returns US https://github.com/electron/electron/issues/2484 */}
+            <DatePicker
+              name="date"
+              selected={this.state.date}
+              onChange={(date) => this.handleDateChange(date)}
+            />
+          </div>
+          <div className={styles.formRow}>
+            <label htmlFor="location">Sample type</label>
+            <input
+              type="text"
+              name="sampleType"
+              defaultValue={this.state.sampleType}
+              onChange={(event) => this.handleChange(event)}
+            />
+          </div>
+          <div className={styles.formRow}>
+            <label>Phenotypic susceptibility</label>
+            {this.susceptibilityPicker()}
+          </div>
+          <div className={styles.formRow}>
+            <label>Patient history</label>
+            <label>
+              <input type="checkbox" name="treatedForTB" defaultChecked={this.state.treatedForTB} onChange={(event) => this.handleCheckboxChange(event)} />
+              Patient has been treated for TB before
+            </label>
+          </div>
+          <div className={styles.formRow}>
+            <label>Help improve Atlas</label>
+            <label>
+              <input type="checkbox" name="shareSequence" defaultChecked={this.state.shareSequence} onChange={(event) => this.handleCheckboxChange(event)} />
+              Share bacterial DNA sequence with Atlas
+            </label>
+          </div>
         </div>
-        <div className={styles.formRow}>
-          <label htmlFor="location">Collection date</label>
-          {/* DatePicker currently shows US locale since Electron returns US https://github.com/electron/electron/issues/2484 */}
-          <DatePicker
-            name="date"
-            selected={this.state.date}
-            onChange={(date) => this.handleDateChange(date)}
-          />
-        </div>
-        <div className={styles.formRow}>
-          <label htmlFor="location">Sample type</label>
-          <input
-            type="text"
-            name="sampleType"
-            defaultValue={this.state.sampleType}
-            onChange={(event) => this.handleChange(event)}
-          />
-        </div>
-        <div className={styles.formRow}>
-          <label>Phenotypic susceptibility</label>
-          {this.susceptibilityPicker()}
-        </div>
-        <div className={styles.formRow}>
-          <label>Patient history</label>
-          <input type="checkbox" name="treatedForTB" defaultChecked={this.state.treatedForTB} onChange={(event) => this.handleCheckboxChange(event)} />
-        </div>
-        <div className={styles.formRow}>
-          <label>Help improve Atlas</label>
-          <input type="checkbox" name="shareSequence" defaultChecked={this.state.shareSequence} onChange={(event) => this.handleCheckboxChange(event)} />
-        </div>
-        {children}
       </div>
     );
   }
