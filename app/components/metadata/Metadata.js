4205a6f3653865637f2ce086ad849221af7d39fe
Simon Heys <si@simonheys.com>
2016-09-01 09:14:40 +0100

    WIP form components

--- app/components/metadata/Metadata.js
+++ app/components/metadata/Metadata.js
@@ -22,8 +22,26 @@ class Metadata extends Component {
     };
   }
 
+  handleDateChange(date) {
+    console.log('handleDateChange', date);
+    this.setState({
+      date
+    });
+  }
+
+  handleCheckboxChange(event) {
+    console.log('handleCheckboxChange', event);
+    var state = {};
+    state[event.target.name] = event.target.checked;
+    console.log('updating state', state);
+    this.setState(state);
+  }
+
   handleChange(event) {
     console.log('handleChange', event);
+    console.log('event.target.name', event.target.name);
+    console.log('event.target.value', event.target.value);
+    console.log('event.target.checked', event.target.checked);
     var state = {};
     state[event.target.name] = event.target.value;
     console.log('updating state', state);
@@ -50,18 +68,39 @@ class Metadata extends Component {
     const {children} = this.props;
     return (
       <div className={styles.container}>
-        {this.locationSelect()}
-        {/* DatePicker currently shows US locale since Electron returns US https://github.com/electron/electron/issues/2484 */}
-        <DatePicker
-          name="date"
-          selected={this.state.date}
-          onChange={(date) => this.handleChange({
-            target: {
-              name: 'date',
-              value: date
-            }})
-          }
-        />
+        <div className={styles.formRow}>
+          <label htmlFor="location">Collection location</label>
+          {this.locationSelect()}
+        </div>
+        <div className={styles.formRow}>
+          <label htmlFor="location">Collection date</label>
+          {/* DatePicker currently shows US locale since Electron returns US https://github.com/electron/electron/issues/2484 */}
+          <DatePicker
+            name="date"
+            selected={this.state.date}
+            onChange={(date) => this.handleDateChange(date)}
+          />
+        </div>
+        <div className={styles.formRow}>
+          <label htmlFor="location">Sample type</label>
+          <input
+            type="text"
+            name="sampleType"
+            defaultValue={this.state.sampleType}
+            onChange={(event) => this.handleChange(event)}
+          />
+        </div>
+        <div className={styles.formRow}>
+          <label>Phenotypic susceptibility</label>
+        </div>
+        <div className={styles.formRow}>
+          <label>Patient history</label>
+          <input type="checkbox" name="treatedForTB" defaultChecked={this.state.treatedForTB} onChange={(event) => this.handleCheckboxChange(event)} />
+        </div>
+        <div className={styles.formRow}>
+          <label>Help improve Atlas</label>
+          <input type="checkbox" name="shareSequence" defaultChecked={this.state.shareSequence} onChange={(event) => this.handleCheckboxChange(event)} />
+        </div>
         {children}
       </div>
     );
