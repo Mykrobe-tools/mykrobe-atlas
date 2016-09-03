f4e9884174bfb5af04ecc340f11a9d04b4863007
Simon Heys <si@simonheys.com>
2016-09-03 17:05:04 +0100

    WIP generate icons

---
+++ scripts/generate-icons.js
@@ -0,0 +1,65 @@
+require('babel-polyfill');
+
+const targets = require('./targets.json');
+const path = require('path');
+const exec = require('child_process').exec;
+
+let promises = [];
+targets.forEach((target, index) => {
+  const iconPath = path.resolve(__dirname, `../resources/icon/${target.value}`);
+  promises.push(generateIconsMac(iconPath));
+});
+
+Promise.all(promises).then(() => {
+  console.log('done');
+})
+.catch((err) => {
+  console.error(err);
+});
+
+function generateIconsMac(iconPath) {
+  let promises = [];
+  const sizes = [16, 32, 128, 256, 512];
+  sizes.forEach((size, index) => {
+    let sizeRetina = size * 2;
+    let filenameBase = `icon_${size}x${size}`;
+    let filename = `${filenameBase}.png`;
+    let filenameRetina = `${filenameBase}@2x.png`;
+    let outputPath = path.join(iconPath, 'icon.iconset', filename);
+    let outputPathRetina = path.join(iconPath, 'icon.iconset', filenameRetina);
+    let pdfPath = path.join(iconPath, 'icon.pdf');
+
+    let command = `gs -dNOPAUSE -dBATCH -sDEVICE=pngalpha -r72 -dPDFFitPage=true -g${size}x${size} -sOutputFile="${outputPath}" "${pdfPath}"`;
+    let commandRetina = `gs -dNOPAUSE -dBATCH -sDEVICE=pngalpha -r72 -dPDFFitPage=true -g${sizeRetina}x${sizeRetina} -sOutputFile="${outputPathRetina}" "${pdfPath}"`;
+
+    promises.push(execute(command));
+    promises.push(execute(commandRetina));
+  });
+  let icnsPath = path.join(iconPath, 'icon.icns');
+  let iconsetPath = path.join(iconPath, 'icon.iconset');
+  let command = 'iconutil -c icns -o "' + icnsPath + '" "' + iconsetPath + '"';
+  promises.push(execute(command));
+  return Promise.all(promises);
+}
+
+function execute(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(stdout);
+    });
+  });
+}
+
+/*
+exec('cat *.js bad_file | wc -l', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+*/
