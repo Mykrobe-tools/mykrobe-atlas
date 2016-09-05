20a4f401d502fc72c682c5249a1233cbbee5090e
Simon Heys <si@simonheys.com>
2016-09-05 13:59:08 +0100

    WIP checking default build

--- package.js
+++ package.js
@@ -45,9 +45,9 @@ if (version) {
 }
 else {
   // use the same version as the currently-installed electron-prebuilt
-  exec('npm list electron-prebuilt --dev', (err, stdout) => {
+  exec('npm list electron-prebuilt --depth=0 --dev', (err, stdout) => {
     if (err) {
-      DEFAULT_OPTS.version = '1.2.0';
+      DEFAULT_OPTS.version = '1.3.5';
     }
     else {
       DEFAULT_OPTS.version = stdout.split('electron-prebuilt@')[1].replace(/\s/g, '');
