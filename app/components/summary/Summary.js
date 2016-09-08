3b2f31693df172b2bfdb1cf7503888e6bf8313e4
Simon Heys <si@simonheys.com>
2016-09-08 11:00:34 +0100

    WIP Summary

--- app/components/summary/Summary.js
+++ app/components/summary/Summary.js
@@ -2,12 +2,32 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import styles from './Summary.css';
 
+class SummaryPanel extends Component {
+  render() {
+    const {title} = this.props;
+    return (
+      <div className={styles.panelContainer}>
+        <div className={styles.panelContent}>
+          {title}
+        </div>
+      </div>
+    );
+  }
+}
+
 class Summary extends Component {
   render() {
     const {children} = this.props;
     return (
       <div className={styles.container}>
-        Summary
+        <div className={styles.title}>
+          Summary
+        </div>
+        <div className={styles.summaryContainer}>
+          <SummaryPanel title="Metadata" />
+          <SummaryPanel title="Resistance Profile" />
+          <SummaryPanel title="Variants Inducing Resistance" />
+        </div>
       </div>
     );
   }
