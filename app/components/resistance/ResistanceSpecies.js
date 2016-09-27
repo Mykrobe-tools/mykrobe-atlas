ed011552655514245e8d9a96cb5b0185d4633a30
Simon Heys <si@simonheys.com>
2016-09-27 17:01:14 +0100

    Drugs

--- app/components/resistance/ResistanceSpecies.js
+++ app/components/resistance/ResistanceSpecies.js
@@ -2,12 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { Route, IndexRoute } from 'react-router';
 import { connect } from 'react-redux';
 import styles from './ResistanceSpecies.css';
-
-import * as AnalyserActions from 'actions/AnalyserActions';
-import MykrobeConfig from 'api/MykrobeConfig';
-import * as TargetConstants from 'constants/TargetConstants';
-
-// TODO: move config into global props
+import Panel from  'components/ui/Panel';
 
 class ResistanceSpecies extends Component {
   render() {
@@ -15,38 +10,15 @@ class ResistanceSpecies extends Component {
     if ( !analyser.transformed ) {
       return null;
     }
-    const config = new MykrobeConfig();
-    const {resistant, lineage, species} = analyser.transformed;
-
-    let drugsResistanceModel = {
-        mdr:false,
-        xdr:false
-    };
-
-    if ( resistant.indexOf('Isoniazid') !== -1 && resistant.indexOf('Rifampicin') !== -1 ) {
-        drugsResistanceModel.mdr = true;
-        /*
-        If MDR AND R to both fluoroquinolones and one of the other these 3 (Amikacin, Kanamycin, Capreomycin), then call it XDR (Extensively Drug Resistant)
-        */
-        if ( resistant.indexOf('Quinolones') ) {
-            if ( resistant.indexOf('Amikacin') !== -1 || resistant.indexOf('Kanamycin') !== -1 || resistant.indexOf('Capreomycin') !== -1 ) {
-                drugsResistanceModel.xdr = true;
-            }
-        }
-    }
-
-    let speciesModel = '';
-
-    if ( TargetConstants.SPECIES_TB === config.species ) {
-        speciesModel = species.join(' / ') +' (lineage: '+lineage[0]+')';
-    }
-    else {
-        speciesModel = species.join(' / ');
-    }
+    const {speciesPretty} = analyser.transformed;
 
     return (
-      <div>
-        {speciesModel}
+      <div className={styles.container}>
+        <Panel title="Species" columns={8}>
+          <div className={styles.species}>
+            {speciesPretty}
+          </div>
+        </Panel>
       </div>
     );
   }
