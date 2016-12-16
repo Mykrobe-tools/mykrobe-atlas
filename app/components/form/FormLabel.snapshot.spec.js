9f6f151f81f1ee0b9b530b4ffa5f5cc8e593a973
Pete Goodman <thegingerbloke@users.noreply.github.com>
2016-12-16 12:28:31 +0000

    Add  snapshot specs for remaininggeneric form components

---
+++ app/components/form/FormLabel.snapshot.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import FormLabel from './FormLabel';
+
+describe('FormLabel component snapshot', () => {
+  const htmlFor = 'loremIpsum';
+  const label = 'Lorem Ipsum';
+  const children = '<a href="#">Dolor Sit Amet</a>';
+
+  it('renders correctly', () => {
+    const component = renderer.create(
+      <FormLabel
+        htmlFor={htmlFor}
+        children={children}
+        label={label} />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
