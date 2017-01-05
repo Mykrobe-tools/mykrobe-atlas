1c12fa55977bdec8fcbb1ebbeac186d1bce99152
Pete Goodman <thegingerbloke@users.noreply.github.com>
2017-01-05 17:17:33 +0000

    Add experiments reducer spec

---
+++ app/reducers/experiments.spec.js
@@ -0,0 +1,44 @@
+import reducer from './experiments';
+import * as ActionTypes from '../constants/ActionTypes';
+
+const initialState = {
+  isFetching: false,
+  samples: []
+};
+
+describe('experiments reducer', () => {
+  it('should return the initial state', () => {
+    expect(
+      reducer(undefined, {})
+    ).toEqual(
+      initialState
+    );
+  });
+
+  it('should handle "REQUEST_EXPERIMENTS" action', () => {
+    expect(
+      reducer(undefined, {
+        type: ActionTypes.REQUEST_EXPERIMENTS
+      })
+    ).toEqual(
+      {
+        isFetching: true,
+        samples: []
+      }
+    );
+  });
+
+  it('should handle "RECEIVE_EXPERIMENTS" action', () => {
+    expect(
+      reducer(undefined, {
+        type: ActionTypes.RECEIVE_EXPERIMENTS,
+        data: [{lorem: 'ipsum'}]
+      })
+    ).toEqual(
+      {
+        isFetching: false,
+        samples: [{lorem: 'ipsum'}]
+      }
+    );
+  });
+});
