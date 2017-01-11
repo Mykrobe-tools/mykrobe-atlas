eafd68ad9742959a5805efc3424335c2fa9cac0b
Simon Heys <si@simonheys.com>
2017-01-11 12:59:17 +0000

    Created M+S API keys

--- app/config.js
+++ app/config.js
@@ -2,9 +2,9 @@
 
 export default {
   'GOOGLE_MAPS_API_KEY': 'AIzaSyAe_EWm97fTPHqzfRrhu2DVwO_iseBQkAc',
-  'BOX_CLIENT_ID': 'gjwt1p1esw2pfwcfkswpxfmtpua0ybka',
-  'DROPBOX_APP_KEY': '0dase5xwg2u4li4',
-  'GOOGLE_DRIVE_CLIENT_ID': '971300101515-nf9vb3c52smc7nudb2cjie7fd21keqj7.apps.googleusercontent.com',
-  'GOOGLE_DRIVE_DEVELOPER_KEY': 'AIzaSyBNgTFyDmqIn9P-ivtXlTD8nfk6Qgn1TBI',
-  'ONEDRIVE_CLIENT_ID': 'f831d53e-8f2f-4446-8d9b-54f57161cb6e'
+  'BOX_CLIENT_ID': 'uudga67zohcl0ttwzd38lc3orwumrppo',
+  'DROPBOX_APP_KEY': 'yavwutzodtyoqjg',
+  'GOOGLE_DRIVE_CLIENT_ID': '780918371602-rqgivkfba07ko1vvu5h868eomc3edq6f.apps.googleusercontent.com',
+  'GOOGLE_DRIVE_DEVELOPER_KEY': 'AIzaSyAGWxdeXCK_1_AGRWZlkoaH6gQCjfC-3pI',
+  'ONEDRIVE_CLIENT_ID': 'e39a152d-61a5-4dbc-981c-95a0d0f23d9f'
 };
