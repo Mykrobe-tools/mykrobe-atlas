d5bb945362b5742b023926d6d5cd0677074b616f
Mark Thomsit <mark@ceremium.com>
2017-01-20 16:12:11 +0000

    Add cleanup for docker images/containers.  Add environment-based deploy location

--- deploy/build.sh
+++ deploy/build.sh
@@ -8,7 +8,7 @@ echo $CURRENT_VERSION > /var/go/releases/CURRENT_VERSION
 mkdir -p /var/go/releases/$CURRENT_VERSION
 cd /var/go/releases/$CURRENT_VERSION
 
-cp -rf /var/lib/go-agent/pipelines/atlas/. .
+cp -rf /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas/. .
 
 # remove current images
 docker ps -a -q | xargs --no-run-if-empty docker rm -f 
