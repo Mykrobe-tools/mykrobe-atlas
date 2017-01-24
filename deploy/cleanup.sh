68c305d1df93ee2993908e4545f356c6c65bf9d4
Mark Thomsit <mark@ceremium.com>
2017-01-24 15:26:54 +0000

    Add cron-based cleanup.  Add rollback

---
+++ deploy/cleanup.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# 0 * * * * /var/lib/go-agent/pipelines/atlas-uat/deploy/cleanup/cleanup.sh
+
+export CURRENT_VERSION=`cat /var/go/releases/CURRENT_VERSION`
+export PREVIOUS_VERSION=`cat /var/go/releases/PREVIOUS_VERSION`
+
+for d in /var/go/releases/*/ ; do
+        if [ "${d}" != "/var/go/releases/${CURRENT_VERSION}/" ] && [ "${d}" != "/var/go/releases/${PREVIOUS_VERSION}/" ]; then
+        		echo "Removing: ${d}"
+                rm -rf "${d}"
+        fi
+done
