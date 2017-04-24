cb93de5dcfa9b2c137714a6858809d47857c618a
Yassire Elhani <yassire@makeandship.com>
2017-04-24 16:32:03 +0100

    separate deployment for the client

--- deploy/rollback.sh
+++ deploy/rollback.sh
@@ -1,16 +1,16 @@
 # remove the failed deployment directory
-CURRENT_VERSION=$(cat /var/go/releases/CURRENT_VERSION)
-rm -R /var/go/releases/$CURRENT_VERSION
+CURRENT_VERSION=$(cat /var/go/releases/client/CURRENT_VERSION)
+rm -R /var/go/releases/client/$CURRENT_VERSION
 
 # remove the failed image
 docker images | grep $CURRENT_VERSION | awk '{print $1}' | xargs --no-run-if-empty docker rmi
 
 # work with the prior working version
-PREVIOUS_VERSION=$(cat /var/go/releases/PREVIOUS_VERSION)
-cd /var/go/releases/$PREVIOUS_VERSION
+PREVIOUS_VERSION=$(cat /var/go/releases/client/PREVIOUS_VERSION)
+cd /var/go/releases/client/$PREVIOUS_VERSION
 
 # remove current images
-docker ps -a -q | xargs --no-run-if-empty docker rm -f 
+docker rm -f atlas-client || true
 docker images -q --filter "dangling=true" | xargs --no-run-if-empty docker rmi
 
 # build the target container
@@ -22,6 +22,3 @@ docker-compose -f deploy/docker-compose.yml up -d
 
 # clean containers down
 ./dkcleanup.sh
-
-# restart the reverse proxy
-sudo service nginx restart
