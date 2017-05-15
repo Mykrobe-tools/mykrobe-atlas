832078089144312b6e4492689db87bafef93197b
Yassire Elhani <yassire@makeandship.com>
2017-05-15 11:35:24 +0100

    fix cleanup script

--- deploy/cleanup.sh
+++ deploy/cleanup.sh
@@ -2,11 +2,20 @@
 
 # 0 * * * * /var/lib/go-agent/pipelines/atlas-uat/deploy/cleanup.sh
 
-export CURRENT_VERSION=`cat /var/go/releases/CURRENT_VERSION`
-export PREVIOUS_VERSION=`cat /var/go/releases/PREVIOUS_VERSION`
+export CLIENT_CURRENT_VERSION=`cat /var/go/releases/client/CURRENT_VERSION`
+export CLIENT_PREVIOUS_VERSION=`cat /var/go/releases/client/PREVIOUS_VERSION`
+export SERVER_CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
+export SERVER_PREVIOUS_VERSION=`cat /var/go/releases/server/PREVIOUS_VERSION`
 
-for d in /var/go/releases/*/ ; do
-        if [ "${d}" != "/var/go/releases/${CURRENT_VERSION}/" ] && [ "${d}" != "/var/go/releases/${PREVIOUS_VERSION}/" ]; then
+for d in /var/go/releases/client/*/ ; do
+        if [ "${d}" != "/var/go/releases/client/${CLIENT_CURRENT_VERSION}/" ] && [ "${d}" != "/var/go/releases/client/${CLIENT_PREVIOUS_VERSION}/" ]; then
+        		echo "Removing: ${d}"
+                rm -rf "${d}"
+        fi
+done
+
+for d in /var/go/releases/server/*/ ; do
+        if [ "${d}" != "/var/go/releases/client/${SERVER_CURRENT_VERSION}/" ] && [ "${d}" != "/var/go/releases/client/${SERVER_PREVIOUS_VERSION}/" ]; then
         		echo "Removing: ${d}"
                 rm -rf "${d}"
         fi
