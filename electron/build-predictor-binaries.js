1ed07211af71fa0c652b1bbe91ecc6c984485e81
Simon Heys <si@simonheys.com>
2017-10-19 17:10:09 +0100

    WIP build script

--- electron/build-predictor-binaries.js
+++ electron/build-predictor-binaries.js
@@ -3,6 +3,9 @@
 import { execSync } from 'child_process';
 import path from 'path';
 import fs from 'fs';
+import os from 'os';
+const pkg = require('../package.json');
+const del = require('del');
 
 let command, folder;
 
@@ -11,15 +14,52 @@ const executeCommand = command => {
   execSync(command, { stdio: [0, 1, 2] });
 };
 
+// checkout repo or pull if it already exists
+
 folder = path.join(__dirname, 'predictor-binaries');
 if (!fs.existsSync(path.join(folder, '.git'))) {
-  command = `git clone --recursive -b add-model-to-output git@github.com:iqbal-lab/Mykrobe-predictor.git ${folder}`;
+  command = `git clone --recursive -b add-model-to-output git@github.com:iqbal-lab/Mykrobe-predictor.git '${folder}'`;
 } else {
-  command = `cd ${folder} && git pull`;
+  command = `cd '${folder}' && git pull && git submodule update --init --recursive`;
 }
 executeCommand(command);
 
-// git clone --recursive git@github.com:iqbal-lab/Mykrobe-predictor.git
-// cd ./mccortex && make
-// pip install git+https://github.com/Phelimb/atlas
-// cd dist && pyinstaller --workpath='./pyinstaller_build/binary_cache' --distpath='./pyinstaller_build' mykrobe_predictor_pyinstaller.spec
+// make mccortex
+
+folder = path.join(__dirname, 'predictor-binaries/mccortex');
+command = `cd '${folder}' && make`;
+executeCommand(command);
+
+// install atlas
+
+command = `pip install git+https://github.com/Phelimb/atlas`;
+executeCommand(command);
+
+// build predictor
+
+folder = path.join(__dirname, 'predictor-binaries/dist');
+command = `cd '${folder}' && pyinstaller --noconfirm --workpath='./pyinstaller_build/binary_cache' --distpath='./pyinstaller_build' mykrobe_predictor_pyinstaller.spec`;
+executeCommand(command);
+
+// copy files
+
+const plat = os.platform();
+const arch = os.arch();
+
+const sourceFolder = path.join(
+  __dirname,
+  'predictor-binaries/dist/pyinstaller_build/mykrobe_predictor'
+);
+const destFolder = path.join(
+  __dirname,
+  `resources/bin/${pkg.targetName}/${plat}-${arch}/bin`
+);
+
+del([
+  `${destFolder}/**`,
+  `!${destFolder}`,
+  `!${destFolder}/.gitignore`,
+]).then(() => {
+  command = `cp -r '${sourceFolder}' '${destFolder}'`;
+  executeCommand(command);
+});
