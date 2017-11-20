3de65c43974872daf636e25c5042a04ea97bab75
Simon Heys <si@simonheys.com>
2017-11-20 15:51:51 +0000

    WIP add slow analyser tests

---
+++ app/services/analyser/AnalyserLocalFile.test.js
@@ -0,0 +1,70 @@
+import AnalyserLocalFile from './AnalyserLocalFile';
+import MykrobeConfig from '../MykrobeConfig';
+import path from 'path';
+
+const BAM_FOLDER = `${process.env.HOME}/Dropbox/bams/`;
+const INCLUDE_SLOW_TESTS =
+  process.env.INCLUDE_SLOW_TESTS && process.env.INCLUDE_SLOW_TESTS === 'true';
+
+const config = new MykrobeConfig();
+
+INCLUDE_SLOW_TESTS && jest.setTimeout(10 * 60 * 1000); // 10 minutes
+
+beforeEach(() => {
+  process.env.NODE_ENV = 'development';
+});
+
+afterEach(() => {
+  delete process.env.NODE_ENV;
+});
+
+describe('AnalyserLocalFile', () => {
+  it('should analyse a json file', done => {
+    const analyser = new AnalyserLocalFile(config);
+    const filePath = path.join(BAM_FOLDER, 'tb', 'C00009037_R00000039.json');
+    analyser
+      .analyseFile(filePath)
+      .on('progress', progress => {
+        console.log('progress', progress);
+      })
+      .on('done', result => {
+        const { transformed } = result;
+        expect(transformed.species).toEqual(['Mycobacterium_tuberculosis']);
+        expect(transformed.lineage).toEqual(['European_American']);
+        expect(transformed.resistant).toEqual([
+          'Rifampicin',
+          'Ethambutol',
+          'Quinolones',
+        ]);
+        done();
+      })
+      .on('error', error => {
+        throw error;
+      });
+  });
+  INCLUDE_SLOW_TESTS &&
+    it('should analyse a bam', done => {
+      const analyser = new AnalyserLocalFile(config);
+      const filePath = path.join(BAM_FOLDER, 'tb', 'C00009037_R00000039.bam');
+      analyser
+        .analyseFile(filePath)
+        .on('progress', progress => {
+          console.log('progress', progress);
+        })
+        .on('done', result => {
+          const { json, transformed } = result;
+          console.log('json', JSON.stringify(json, null, 2));
+          expect(transformed.species).toEqual(['Mycobacterium_tuberculosis']);
+          expect(transformed.lineage).toEqual(['European_American']);
+          expect(transformed.resistant).toEqual([
+            'Rifampicin',
+            'Ethambutol',
+            'Quinolones',
+          ]);
+          done();
+        })
+        .on('error', error => {
+          throw error;
+        });
+    });
+});
