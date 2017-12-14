2fc21321ae08df2a3bd10e8e95326dc629f0e8b8
Simon Heys <si@simonheys.com>
2017-12-14 16:48:18 +0000

    Inject value into form field

--- app/index.e2e.test.js
+++ app/index.e2e.test.js
@@ -9,51 +9,59 @@ jest.setTimeout(10 * 60 * 1000); // 10 minutes
 
 const delay = time => new Promise(resolve => setTimeout(resolve, time));
 
+// sendKeys() doesn't seem to work for form fields, so inject values using js
+const setValueForSelector = async (selector, value) => {
+  await driver.executeScript(
+    `document.querySelectorAll('${selector}')[0].value='${value}';`
+  );
+};
+
 describe('Web e2e prerequisites', () => {
   it('should contain a test', done => {
     done();
   });
 });
 
+let driver, child;
+
 INCLUDE_SLOW_TESTS &&
   describe('Web e2e main window', function spec() {
     beforeAll(() => {
-      this.driver = new webdriver.Builder().forBrowser('chrome').build();
-      this.child = exec('yarn web-hot-server');
-      // this.child = exec('yarn web-build && yarn web-build-simple-server');
+      driver = new webdriver.Builder().forBrowser('chrome').build();
+      child = exec('yarn web-hot-server');
+      // child = exec('yarn web-build && yarn web-build-simple-server');
     });
 
     afterAll(() => {
-      this.child.kill();
-      this.driver.quit();
+      child.kill();
+      driver.quit();
     });
 
     it('should open website', async () => {
       // wait a few seconds for the server to boot up
       await delay(3000);
       // ask the browser to open a page
-      await this.driver.navigate().to('http://localhost:3000/');
+      await driver.navigate().to('http://localhost:3000/');
     });
 
     it('should fail to log in', async () => {
-      await this.driver.wait(
+      await driver.wait(
         until.elementLocated(By.css('[data-tid="button-log-in"]'))
       );
-      await this.driver
-        .findElement(By.css('[data-tid="button-log-in"]'))
-        .click();
-      await this.driver.wait(
+      await driver.findElement(By.css('[data-tid="button-log-in"]')).click();
+      await driver.wait(
         until.elementLocated(By.css('[data-tid="input-email"]'))
       );
-      await this.driver
-        .findElement(By.css('[data-tid="input-email"]'))
-        .sendKeys('simon@makeandship.com');
-      await this.driver
-        .findElement(By.css('[data-tid="input-password"]'))
-        .sendKeys('password');
 
-      await this.driver
-        .findElement(By.css('[data-tid="button-submit"]'))
-        .click();
+      await setValueForSelector(
+        '[data-tid="input-email"]',
+        'simon@makeandship.com'
+      );
+
+      await setValueForSelector('[data-tid="input-password"]', 'password');
+
+      await driver.findElement(By.css('[data-tid="button-submit"]')).click();
+
+      // FIXME: there is an issue here with infinite redirect loop on 401
     });
   });
