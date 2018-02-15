db771d97bd64327a09029bc8a5cd0f22ee79c268
Simon Heys <si@simonheys.com>
2018-02-15 07:59:49 +0000

    WIP (not working) refactor modules

--- app/modules/index.js
+++ app/modules/index.js
@@ -3,15 +3,17 @@
 import { combineReducers } from 'redux';
 
 import analyser from './analyser';
-import notifications from './notifications';
 import auth from './auth';
 import experiments from './experiments';
+import metadata from './metadata';
+import notifications from './notifications';
 
 const reducer = combineReducers({
   analyser,
-  notifications,
   auth,
   experiments,
+  metadata,
+  notifications,
 });
 
 export default reducer;
