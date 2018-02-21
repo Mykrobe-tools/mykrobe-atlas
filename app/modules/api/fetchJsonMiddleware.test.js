b95050d267127a94782e858e5a25a5e2a1bcf18b
Simon Heys <si@simonheys.com>
2018-02-21 07:47:56 +0000

    WIP middleware tests

---
+++ app/modules/api/fetchJsonMiddleware.test.js
@@ -0,0 +1,46 @@
+/* @flow */
+
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+
+import { FETCH_JSON, fetchJsonMiddleware } from './fetchJsonMiddleware';
+import { BASE_URL } from '../../constants/APIConstants.js';
+
+const REQUEST = 'REQUEST';
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
+
+const createMockStore = configureMockStore([thunk, fetchJsonMiddleware]);
+
+const initialState = {};
+
+describe('fetchJsonMiddleware', () => {
+  const store = createMockStore(initialState);
+
+  beforeEach(() => {
+    store.clearActions();
+    nock.cleanAll();
+  });
+
+  it('should handle basic request', async () => {
+    const payload = { test: true };
+    nock(BASE_URL)
+      .get('/test/fetchJsonMiddleware')
+      .query(true)
+      .reply(200, { status: 'success', data: payload });
+    await store.dispatch({
+      [FETCH_JSON]: {
+        url: `${BASE_URL}/test/fetchJsonMiddleware`,
+        types: [REQUEST, SUCCESS, FAILURE],
+      },
+    });
+    const dispatchedActions = store.getActions();
+    expect(dispatchedActions).toMatchSnapshot();
+    expect(dispatchedActions[1].payload).toEqual(payload);
+    console.log(
+      'dispatchedActions',
+      JSON.stringify(dispatchedActions, null, 2)
+    );
+  });
+});
