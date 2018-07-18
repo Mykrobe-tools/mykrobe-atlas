53129a8450fb3a7ea6427e06553fe96a7c494243
Simon Heys <si@simonheys.com>
2018-07-18 10:05:26 +0100

    Feature/upload button (#49)
    * General purpose upload button

--- app/components/upload/Upload.js
+++ app/components/upload/Upload.js
@@ -7,82 +7,15 @@ import { connect } from 'react-redux';
 import styles from './Upload.scss';
 import AnimatedBackground from '../animatedbackground/AnimatedBackground';
 import Logo from '../logo/Logo';
-import PopoverMenu from '../ui/PopoverMenu';
 import Header from '../header/Header';
+
+import UploadButton from './UploadButton';
 
 import { getIsAuthenticated } from 'makeandship-js-common/src/modules/auth';
 
-import {
-  uploadFileAssignBrowse,
-  uploadDropbox,
-  uploadGoogleDrive,
-  uploadBox,
-  uploadOneDrive,
-} from '../../modules/upload';
-
 class Upload extends React.Component<*> {
-  _uploadButton: Element;
-
-  setUploadButtonRef = (ref: ?Element) => {
-    if (!ref) {
-      return;
-    }
-    this._uploadButton = ref;
-    const { uploadFileAssignBrowse } = this.props;
-    uploadFileAssignBrowse(this._uploadButton);
-  };
-
-  popoverMenuLinks() {
-    const {
-      uploadDropbox,
-      uploadGoogleDrive,
-      uploadBox,
-      uploadOneDrive,
-    } = this.props;
-    return [
-      {
-        text: 'Computer',
-        onClick: (e: Event) => {
-          e.preventDefault();
-          if (this._uploadButton) {
-            this._uploadButton.click();
-          }
-        },
-      },
-      {
-        text: 'Dropbox',
-        onClick: (e: Event) => {
-          e.preventDefault();
-          uploadDropbox();
-        },
-      },
-      {
-        text: 'Box',
-        onClick: (e: Event) => {
-          e.preventDefault();
-          uploadBox();
-        },
-      },
-      {
-        text: 'Google Drive',
-        onClick: (e: Event) => {
-          e.preventDefault();
-          uploadGoogleDrive();
-        },
-      },
-      {
-        text: 'OneDrive',
-        onClick: (e: Event) => {
-          e.preventDefault();
-          uploadOneDrive();
-        },
-      },
-    ];
-  }
-
   render() {
     const { isAuthenticated } = this.props;
-    const popoverMenuLinks = this.popoverMenuLinks();
     return (
       <div className={styles.container} data-tid="component-upload">
         <AnimatedBackground />
@@ -97,14 +30,7 @@ class Upload extends React.Component<*> {
             </div>
             {isAuthenticated && (
               <div className={styles.buttonContainer}>
-                <span
-                  className={styles.buttonOffscreen}
-                  ref={this.setUploadButtonRef}
-                />
-                <PopoverMenu
-                  toggleText="Analyse Sample"
-                  links={popoverMenuLinks}
-                />
+                <UploadButton size="lg" />
               </div>
             )}
           </div>
@@ -117,24 +43,10 @@ class Upload extends React.Component<*> {
 
 Upload.propTypes = {
   isAuthenticated: PropTypes.bool.isRequired,
-  uploadFileAssignBrowse: PropTypes.func.isRequired,
-  uploadDropbox: PropTypes.func.isRequired,
-  uploadGoogleDrive: PropTypes.func.isRequired,
-  uploadBox: PropTypes.func.isRequired,
-  uploadOneDrive: PropTypes.func.isRequired,
 };
 
-const withRedux = connect(
-  state => ({
-    isAuthenticated: getIsAuthenticated(state),
-  }),
-  {
-    uploadFileAssignBrowse,
-    uploadDropbox,
-    uploadGoogleDrive,
-    uploadBox,
-    uploadOneDrive,
-  }
-);
+const withRedux = connect(state => ({
+  isAuthenticated: getIsAuthenticated(state),
+}));
 
 export default withRedux(Upload);
