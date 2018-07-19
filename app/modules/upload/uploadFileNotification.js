2fc56e5b3b76e72292d803580ee92cfc73101304
Simon Heys <si@simonheys.com>
2018-07-19 13:31:03 +0100

    Cleanup

--- app/modules/upload/uploadFileNotification.js
+++ app/modules/upload/uploadFileNotification.js
@@ -1,29 +1,8 @@
 /* @flow */
 
 import { channel } from 'redux-saga';
-import {
-  all,
-  fork,
-  put,
-  take,
-  race,
-  takeEvery,
-  select,
-  call,
-  apply,
-} from 'redux-saga/effects';
-import { createSelector } from 'reselect';
+import { all, fork, put, take, takeEvery, select } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
-
-import {
-  INITIALISE_SUCCESS,
-  SET_TOKEN,
-  CHECK_TOKEN_SUCCESS,
-  REFRESH_TOKEN_SUCCESS,
-  REFRESH_TOKEN_FAIL,
-  getAccessToken,
-  checkToken,
-} from 'makeandship-js-common/src/modules/auth/auth';
 
 import {
   showNotification,
@@ -31,33 +10,33 @@ import {
   NotificationCategories,
 } from '../notifications';
 
-import * as APIConstants from '../../constants/APIConstants';
 import {
-  createExperimentId,
-  deleteExperiment,
-} from '../experiments/experiment';
-
-import ResumableUpload, {
-  RESUMABLE_UPLOAD_FILE_ADDED,
   RESUMABLE_UPLOAD_PROGRESS,
   RESUMABLE_UPLOAD_ERROR,
   RESUMABLE_UPLOAD_DONE,
 } from './util/ResumableUpload';
 
-import ComputeChecksums, {
-  COMPUTE_CHECKSUMS_PROGRESS,
-  COMPUTE_CHECKSUMS_COMPLETE,
-} from './util/ComputeChecksums';
+import { COMPUTE_CHECKSUMS_PROGRESS } from './util/ComputeChecksums';
 
 import {
   SET_FILE_NAME,
   UPLOAD_FILE_CANCEL_SUCCESS,
+  uploadFileCancel,
   getExperimentId,
   getFileName,
   getProgress,
 } from './uploadFile';
 
 // file added
+
+const _interactionChannel = channel();
+
+export function* interactionChannelWatcher(): Generator<*, *, *> {
+  while (true) {
+    const action = yield take(_interactionChannel);
+    yield put(action);
+  }
+}
 
 function* fileAddedWatcher() {
   yield takeEvery(SET_FILE_NAME, function*() {
@@ -70,6 +49,27 @@ function* fileAddedWatcher() {
         content: `Added ${fileName}`,
         progress: 0,
         autoHide: false,
+        actions: [
+          {
+            title: 'View',
+            onClick: () => {
+              _interactionChannel.put(push(`/experiments/${experimentId}`));
+            },
+          },
+          {
+            title: 'Cancel',
+            onClick: () => {
+              if (
+                !confirm(
+                  `Cancel upload? This will also delete any associated metadata.`
+                )
+              ) {
+                return;
+              }
+              _interactionChannel.put(uploadFileCancel());
+            },
+          },
+        ],
       })
     );
   });
@@ -85,7 +85,7 @@ function* computeChecksumsProgressWatcher() {
     yield put(
       updateNotification(experimentId, {
         category: NotificationCategories.MESSAGE,
-        content: `Calculating checksums for ${fileName}`,
+        content: `Preparing ${fileName}`,
         progress,
       })
     );
@@ -93,6 +93,21 @@ function* computeChecksumsProgressWatcher() {
 }
 
 // upload events
+
+function* resumableUploadProgressWatcher() {
+  yield takeEvery(RESUMABLE_UPLOAD_PROGRESS, function*() {
+    const experimentId = yield select(getExperimentId);
+    const fileName = yield select(getFileName);
+    const progress = yield select(getProgress);
+    yield put(
+      updateNotification(experimentId, {
+        category: NotificationCategories.MESSAGE,
+        content: `Uploading ${fileName}`,
+        progress,
+      })
+    );
+  });
+}
 
 function* resumableUploadDoneWatcher() {
   yield takeEvery(RESUMABLE_UPLOAD_DONE, function*() {
@@ -121,8 +136,10 @@ export function* uploadFileNotificationSaga(): Generator<*, *, *> {
   yield all([
     fork(fileAddedWatcher),
     fork(computeChecksumsProgressWatcher),
+    fork(resumableUploadProgressWatcher),
     fork(resumableUploadDoneWatcher),
     fork(resumableUploadErrorWatcher),
     fork(uploadFileCancelWatcher),
+    fork(interactionChannelWatcher),
   ]);
 }
