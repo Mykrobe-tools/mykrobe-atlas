8e78826cd2fcd57d698fdeb890de62867ea8fd61
Simon Heys <si@simonheys.com>
2018-07-23 14:21:48 +0100

    Progress placeholder

--- app/components/metadata/ExperimentMetadataNavigation.js
+++ app/components/metadata/ExperimentMetadataNavigation.js
@@ -10,9 +10,28 @@ import styles from './ExperimentMetadataNavigation.scss';
 class ExperimentMetadataNavigation extends React.Component<*> {
   render() {
     const { match } = this.props;
+    const percent = 33;
     return (
       <div className={styles.container}>
         <Container fluid>
+          <div className={styles.progress}>
+            <div className={styles.percent}>
+              <div className={styles.percentContent}>
+                <span>
+                  <span className={styles.percentValue}>{percent}</span>
+                  <span className={styles.percentSign}>%</span>
+                </span>
+              </div>
+            </div>
+            <div className={styles.title}>
+              <div className={styles.titleHead}>
+                Complete metadata benefits everyone
+              </div>
+              <div className={styles.titleSubhead}>
+                Help Atlas provide more accurate results
+              </div>
+            </div>
+          </div>
           <div className={styles.navigation}>
             <NavLink
               to={`${match.url}/patient`}
