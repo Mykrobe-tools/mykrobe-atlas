54a60a600c31ab158c1cceba471be460586bf0d3
Simon Heys <si@simonheys.com>
2018-07-24 10:26:48 +0100

    Strip attributes from schema

---
+++ app/schemas/experiment/experiment.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import experiment from 'mykrobe-atlas-api/src/schemas/experiment';
+
+const experimentSchema = {
+  ...experiment,
+  $id: undefined,
+  $schema: undefined,
+};
+
+export default experimentSchema;
