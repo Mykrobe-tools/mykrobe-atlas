0373b1f557aab7352832e6fb54d6c86a17746e87
Simon Heys <si@simonheys.com>
2018-08-23 11:09:14 +0100

    Feature/numeric range (#97)
    * Initial implementation
    
    * Styling

--- app/components/choicesFilters/ChoicesFilters.js
+++ app/components/choicesFilters/ChoicesFilters.js
@@ -11,6 +11,7 @@ import styles from './ChoicesFilters.scss';
 import type { Choice } from './types';
 import ChoiceFilterSelect from './ChoiceFilterSelect';
 import ChoiceFilterDateRange from './ChoiceFilterDateRange';
+import ChoiceFilterNumericRange from './ChoiceFilterNumericRange';
 import { titleForChoice } from './util';
 
 type State = {
@@ -118,7 +119,7 @@ class ChoicesFilters extends React.Component<*, State> {
     }
     if (choice.min && choice.max) {
       if (isNumeric(choice.min) && isNumeric(choice.max)) {
-        component = this.renderNumericRange(choiceKey, placeholder);
+        component = <ChoiceFilterNumericRange {...props} />;
       } else {
         component = <ChoiceFilterDateRange {...props} />;
       }
@@ -146,13 +147,6 @@ class ChoicesFilters extends React.Component<*, State> {
         </a>
       </div>
     );
-  };
-
-  // TODO: refactor into individual components
-
-  // eslint-disable-next-line
-  renderNumericRange = (choiceKey: string, placeholder: boolean) => {
-    return 'TODO: numeric range';
   };
 
   render() {
