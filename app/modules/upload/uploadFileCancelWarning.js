38a8a7163fdf7918091c4fa86f0b154a521c988c
Simon Heys <si@simonheys.com>
2018-09-03 16:12:12 +0100

    Prompt to cancel closing window / tab while uploading

---
+++ app/modules/upload/uploadFileCancelWarning.js
@@ -0,0 +1,41 @@
+/* @flow */
+
+import { all, fork, takeEvery, apply } from 'redux-saga/effects';
+import type { Saga } from 'redux-saga';
+
+import {
+  RESUMABLE_UPLOAD_ERROR,
+  RESUMABLE_UPLOAD_DONE,
+} from './util/ResumableUpload';
+
+import {
+  UPLOAD_FILE,
+  UPLOAD_FILE_DROP,
+  UPLOAD_FILE_CANCEL,
+} from './uploadFile';
+
+export const _warn = (e: any) => {
+  e.preventDefault();
+  const confirmationMessage = 'Unfinished upload in progress - are you sure?';
+  (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+  return confirmationMessage;
+};
+
+function* addWarningWatcher() {
+  yield takeEvery([UPLOAD_FILE_DROP, UPLOAD_FILE], function*() {
+    yield apply(window, 'addEventListener', ['beforeunload', _warn]);
+  });
+}
+
+function* removeWarningWatcher() {
+  yield takeEvery(
+    [UPLOAD_FILE_CANCEL, RESUMABLE_UPLOAD_DONE, RESUMABLE_UPLOAD_ERROR],
+    function*() {
+      yield apply(window, 'removeEventListener', ['beforeunload', _warn]);
+    }
+  );
+}
+
+export function* uploadFileCancelWarningSaga(): Saga {
+  yield all([fork(addWarningWatcher), fork(removeWarningWatcher)]);
+}
