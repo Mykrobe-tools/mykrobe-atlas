94e36d0177a84c375b1467073be67070eb13ba3f
Simon Heys <si@simonheys.com>
2019-05-01 14:44:46 +0100

    Update share lib, storybook config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,9 @@
-const webpack = require('webpack');
-const base = require('../webpack.config.development');
-
-module.exports = {
-  mode: 'development',
-  plugins: [
-    new webpack.DefinePlugin({
-      IS_ELECTRON: JSON.stringify(false),
-    }),
-  ],
-  resolve: {
-    ...base.resolve,
-  },
-  module: {
-    rules: [...base.module.rules],
-  },
+module.exports = async ({ config, mode }) => {
+  const customConfig =
+    mode === 'DEVELOPMENT'
+      ? require('../web/webpack.config.development')
+      : require('../web/webpack.config.production');
+  config.resolve = { ...config.resolve, ...customConfig.resolve };
+  config.module.rules = [...config.module.rules, ...customConfig.module.rules];
+  return config;
 };
