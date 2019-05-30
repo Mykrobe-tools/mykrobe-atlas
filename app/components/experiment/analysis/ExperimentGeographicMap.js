ce9f440b8851b3fc1a7bf9c22b29ee3cf81edde3
Simon Heys <si@simonheys.com>
2019-05-30 15:29:05 +0100

    Basic inline marker

--- app/components/experiment/analysis/ExperimentGeographicMap.js
+++ app/components/experiment/analysis/ExperimentGeographicMap.js
@@ -15,6 +15,37 @@ import styles from './ExperimentGeographicMap.scss';
 export const DEFAULT_LAT = 51.5074;
 export const DEFAULT_LNG = 0.1278;
 
+export const COLOR_RED = '#c30042';
+export const COLOR_BLUE = '#0f82d0';
+
+export const makeSvgMarker = ({
+  color = COLOR_RED,
+  diameter = 32,
+  strokeWidth = 4,
+}: {
+  color?: string,
+  diameter?: number,
+  strokeWidth?: number,
+}) => {
+  const radius = 0.5 * diameter - 0.5 * strokeWidth;
+  const svg = `\
+<svg viewBox="0 0 ${diameter} ${diameter}" xmlns="http://www.w3.org/2000/svg">
+  <circle fill="${color}" stroke="white" stroke-width="4" cx="${0.5 *
+    diameter}" cy="${0.5 * diameter}" r="${radius}"/>
+</svg>`;
+  const svgEncoded = window.btoa(svg);
+  const dataBase64 = `data:image/svg+xml;base64,${svgEncoded}`;
+  return dataBase64;
+};
+
+/*
+ var getGoogleClusterInlineSvg = function (color) {
+        var encoded = window.btoa('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-100 -100 200 200"><defs><g id="a" transform="rotate(45)"><path d="M0 47A47 47 0 0 0 47 0L62 0A62 62 0 0 1 0 62Z" fill-opacity="0.7"/><path d="M0 67A67 67 0 0 0 67 0L81 0A81 81 0 0 1 0 81Z" fill-opacity="0.5"/><path d="M0 86A86 86 0 0 0 86 0L100 0A100 100 0 0 1 0 100Z" fill-opacity="0.3"/></g></defs><g fill="' + color + '"><circle r="42"/><use xlink:href="#a"/><g transform="rotate(120)"><use xlink:href="#a"/></g><g transform="rotate(240)"><use xlink:href="#a"/></g></g></svg>');
+
+        return ('data:image/svg+xml;base64,' + encoded);
+    };
+*/
+
 class ExperimentGeographicMap extends React.Component<*> {
   _google: Object;
   _map: Object;
@@ -50,12 +81,13 @@ class ExperimentGeographicMap extends React.Component<*> {
     this._map = new this._google.maps.Map(this._mapDiv, options);
     // https://googlemaps.github.io/js-marker-clusterer/docs/reference.html
     this._markerClusterer = new MarkerClusterer(this._map, [], {
-      imagePath:
-        'https://raw.githubusercontent.com/googlemaps/js-marker-clusterer/gh-pages/images/m',
       styles: [
         {
           textColor: 'white',
           textSize: 16,
+          width: 48,
+          height: 48,
+          url: makeSvgMarker({ diameter: 48 }),
         },
       ],
     });
@@ -146,7 +178,7 @@ class ExperimentGeographicMap extends React.Component<*> {
             path: this._google.maps.SymbolPath.CIRCLE,
             scale: 10,
             strokeWeight: 4,
-            fillColor: index === 0 ? '#c30042' : '#0f82d0',
+            fillColor: index === 0 ? COLOR_RED : COLOR_BLUE,
             strokeColor: '#fff',
             fillOpacity: 1,
           },
