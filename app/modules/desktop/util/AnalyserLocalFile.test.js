ffa9ceeb64388a7c1ff458265a9aab031735eed7
Simon Heys <si@simonheys.com>
2020-01-06 14:45:19 +0000

    WIP progress test

--- app/modules/desktop/util/AnalyserLocalFile.test.js
+++ app/modules/desktop/util/AnalyserLocalFile.test.js
@@ -51,13 +51,15 @@ const pathToProcessMock = path.join(
 
 describe('AnalyserLocalFile', () => {
   it(`should handle exit with code 123`, async done => {
-    const child = exec(`babel-node "${pathToProcessMock}" --exitWithCode 123`);
+    const command = `babel-node "${pathToProcessMock}" --exitWithCode 123`;
+    console.log('command', command);
+    const child = exec(command);
     const analyser = new AnalyserLocalFile();
     analyser
       .setChildProcess(child)
       .monitorChildProcess()
       .on('done', result => {
-        console.log(result);
+        console.log('result', result);
         throw 'Analyser should not emit done';
       })
       .on('error', error => {
@@ -68,6 +70,32 @@ describe('AnalyserLocalFile', () => {
         done();
       });
   });
+
+  it(`should handle progress`, async done => {
+    const json = { progress: 'true' };
+    const jsonString = JSON.stringify({ progress: 'true' });
+    const command = `babel-node "${pathToProcessMock}" --progress --emit '${jsonString}'`;
+    console.log('command', command);
+    const child = exec(command);
+    const analyser = new AnalyserLocalFile();
+    analyser
+      .setChildProcess(child)
+      .monitorChildProcess()
+      .on('progress', progress => {
+        console.log('progress', progress);
+      })
+      .on('done', result => {
+        console.log('result', result);
+        console.log('json', json);
+        expect(result.json).toEqual(json);
+        done();
+      })
+      .on('error', error => {
+        console.error('error', error);
+        throw 'Analyser should not emit error';
+      });
+  });
+
   // it(`should handle exit with code 123`, async done => {
   //   console.log('pathToProcessMock', pathToProcessMock);
   //   const child = exec(`babel-node "${pathToProcessMock}" --exitWithCode 123`);
