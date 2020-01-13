21141efb560384d5b8340dd8c7e3d618837b881a
Simon Heys <si@simonheys.com>
2020-01-13 10:09:28 +0000

    Simplify with hooks

--- app/components/ui/search/SearchNavigation.js
+++ app/components/ui/search/SearchNavigation.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { Container, Row, Col } from 'reactstrap';
 
 import { styles as pageHeaderStyles } from 'makeandship-js-common/src/components/ui/PageHeader';
@@ -10,73 +9,48 @@ import SearchInput from './SearchInput';
 
 import styles from './SearchNavigation.scss';
 
-type State = {
-  q: ?string,
-};
+const SearchNavigation = ({
+  q,
+  onSubmit,
+  title,
+  placeholder,
+}: React.ElementProps<*>): React.Element<*> => {
+  const [stateQ, setStateQ] = React.useState(q);
 
-class SearchNavigation extends React.Component<*, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      q: props.q,
-    };
-  }
-
-  componentDidUpdate = (prevProps: any) => {
-    if (prevProps.q !== this.props.q) {
-      this.setState({
-        q: this.props.q,
-      });
-    }
-  };
-
-  onChange = (e: any) => {
+  const handleOnChange = (e: any) => {
     const q = e.target.value;
-    this.setState({
-      q,
-    });
+    setStateQ(q);
   };
 
-  onSubmit = (e: any) => {
+  const handleOnSubmit = (e: any) => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    let { q } = this.state;
+    let result = stateQ;
     // clear the parameter when empty string
     if (typeof q === 'string' && q.length === 0) {
-      q = undefined;
+      result = undefined;
     }
-    onSubmit && onSubmit(q);
+    onSubmit && onSubmit(result);
   };
 
-  render() {
-    const { title, placeholder } = this.props;
-    const { q } = this.state;
-    return (
-      <Container className={styles.container} fluid>
-        <Row className={styles.rowContainer}>
-          <Col md={3}>
-            <div className={pageHeaderStyles.title}>{title}</div>
-          </Col>
-          <Col md={6} className={styles.searchContainer}>
-            <SearchInput
-              value={q}
-              placeholder={placeholder}
-              onChange={this.onChange}
-              onSubmit={this.onSubmit}
-            />
-          </Col>
-          <Col md={3} />
-        </Row>
-      </Container>
-    );
-  }
-}
-
-SearchNavigation.propTypes = {
-  onChange: PropTypes.func,
-  q: PropTypes.string,
-  title: PropTypes.string,
-  placeholder: PropTypes.string,
+  return (
+    <Container className={styles.container} fluid>
+      <pre>{JSON.stringify(stateQ, null, 2)}</pre>
+      <Row className={styles.rowContainer}>
+        <Col md={3}>
+          <div className={pageHeaderStyles.title}>{title}</div>
+        </Col>
+        <Col md={6} className={styles.searchContainer}>
+          <SearchInput
+            value={stateQ}
+            placeholder={placeholder}
+            onChange={handleOnChange}
+            onSubmit={handleOnSubmit}
+          />
+        </Col>
+        <Col md={3} />
+      </Row>
+    </Container>
+  );
 };
 
 export default SearchNavigation;
