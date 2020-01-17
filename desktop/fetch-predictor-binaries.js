ca3d5ac13a1b191b8c1b45c3df80b8bf6a0e6851
Simon Heys <si@simonheys.com>
2020-01-17 14:46:55 +0000

    Identify downloads

--- desktop/fetch-predictor-binaries.js
+++ desktop/fetch-predictor-binaries.js
@@ -3,6 +3,36 @@
 import { fetchGitHubReleases } from './util/gitHub';
 
 (async () => {
-  const releases = await fetchGitHubReleases();
-  console.log('releases', JSON.stringify(releases, null, 2));
+  const releases = await fetchGitHubReleases({
+    owner: 'martinghunt',
+    repo: 'mykrobe',
+  });
+  // use the first release
+  const release = releases[0];
+  console.log('release', JSON.stringify(release, null, 2));
+  const tag = release.tag_name;
+  console.log(`Release tag ${tag}`);
+  const downloadForPlatforms = {};
+  release.assets.forEach(asset => {
+    if (asset.browser_download_url) {
+      // macOS binary? - mykrobe.command_line.osx.v1.0.6.tar.gz
+      if (
+        asset.browser_download_url.includes(
+          `mykrobe.command_line.osx.${tag}.tar.gz`
+        )
+      ) {
+        downloadForPlatforms['darwin-x64'] = asset.browser_download_url;
+      } else if (
+        asset.browser_download_url.includes(
+          `mykrobe.command_line.windows.${tag}.tar.gz`
+        )
+      ) {
+        downloadForPlatforms['win32-x64'] = asset.browser_download_url;
+      }
+    }
+  });
+  console.log(
+    'downloadForPlatforms',
+    JSON.stringify(downloadForPlatforms, null, 2)
+  );
 })();
