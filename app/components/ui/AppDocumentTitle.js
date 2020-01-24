2036dda34832bd1c802091453e0a2af7ebe0080f
Simon Heys <si@simonheys.com>
2020-01-24 12:17:46 +0000

    Feature/1028 inject vars (#211)
    * Revert approach

--- app/components/ui/AppDocumentTitle.js
+++ app/components/ui/AppDocumentTitle.js
@@ -6,7 +6,7 @@ import DocumentTitle from 'react-document-title';
 
 import { isString, isArray } from 'makeandship-js-common/src/util/is';
 
-const { productName } = require('../../../package.json');
+const { productName } = PACKAGE_JSON;
 
 const AppDocumentTitle = ({
   title,
--- decls/globals.js
+++ decls/globals.js
@@ -1,2 +1,3 @@
 declare var IS_ELECTRON: boolean;
+declare var PACKAGE_JSON: any;
 declare var EventSourcePolyfill: any;
