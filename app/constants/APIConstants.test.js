5f8480f36edf86a81a54a87ba87dded93368a8c0
Simon Heys <si@simonheys.com>
2020-04-29 15:14:45 +0100

    Fix/1066 mac app mis identifying counterpart bam files (#214)
    * Add failing test case and additional extensions
    
    * Rework, passing tests
    
    * Extensions
    
    * Fix browserlist warnings

--- app/constants/APIConstants.test.js
+++ app/constants/APIConstants.test.js
@@ -5,18 +5,30 @@ import * as APIConstants from './APIConstants';
 describe('APIConstants', () => {
   it('Defines API_SAMPLE_EXTENSIONS correctly', () => {
     expect(APIConstants.API_SAMPLE_EXTENSIONS_ARRAY).toEqual([
+      'bai',
       'bam',
-      'gz',
+      'fa',
+      'fasta',
       'fastq',
+      'fna',
+      'fq',
+      'gz',
     ]);
     expect(APIConstants.API_SAMPLE_EXTENSIONS_ARRAY_WITH_DOTS).toEqual([
+      '.bai',
       '.bam',
-      '.gz',
+      '.fa',
+      '.fasta',
       '.fastq',
+      '.fna',
+      '.fq',
+      '.gz',
     ]);
     expect(APIConstants.API_SAMPLE_EXTENSIONS_STRING_WITH_DOTS).toEqual(
-      '.bam,.gz,.fastq'
+      '.bai,.bam,.fa,.fasta,.fastq,.fna,.fq,.gz'
     );
-    expect(APIConstants.API_SAMPLE_EXTENSIONS_STRING).toEqual('bam,gz,fastq');
+    expect(APIConstants.API_SAMPLE_EXTENSIONS_STRING).toEqual(
+      'bai,bam,fa,fasta,fastq,fna,fq,gz'
+    );
   });
 });
