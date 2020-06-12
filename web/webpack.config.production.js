313283d5817f48796db2224d2b774810fc82cd10
Simon Heys <si@simonheys.com>
2020-06-12 06:53:07 +0100

    Build location

--- web/webpack.config.production.js
+++ web/webpack.config.production.js
@@ -4,11 +4,17 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 const webpackConfig = require('../webpack.config.production');
 const pluginConfig = require('./webpack.plugin.config');
 
 module.exports = merge(webpackConfig, {
+  output: {
+    path: path.resolve(__dirname, 'build/static'),
+    publicPath: '/',
+  },
+
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin(pluginConfig.getHtmlWebpackPluginConfig()),
