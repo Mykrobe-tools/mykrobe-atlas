c3c101b0263bae68a43a61b48a0402400892b88b
Yassire Elhani <yassire@makeandship.com>
2020-06-15 13:20:01 +0100

    upgrade docker file (#219)
    * upgrade docker file
    
    * wip
    
    * fix the prod build
    
    * init cloudbuild deployment
    
    * init cloudbuild deployment
    
    * init cloudbuild deployment
    
    * add babel and forever
    
    * add babel and forever
    
    * add babel and forever
    
    * add babel and forever
    
    * upgrade version
    
    * wip
    
    * wip
    
    * wip
    
    * change kc realm
    
    * remove babel-cli
    
    * remove not needed steps
    
    * Build web bundle.js in separate step
    
    * remove not needed steps
    
    * Split install and web-build
    
    * Build server
    
    * Server deploy
    
    * Update runnable
    
    * Version
    
    * Remove old yarn.lock
    
    * Remove unused deployment files
    
    * Update cloud deployment - verify server node_modules
    
    * Move deployment server to own directory
    
    * Clean up build
    
    * Add cors
    
    * Version
    
    * Add ejs to deploy server
    
    * Version
    
    * Ejs directory
    
    * Ejs directory
    
    * Deployment assets in web/
    
    * Deployment assets in web/
    
    * Remove unused deploy files
    
    Co-authored-by: mthomsit <mark@makeandship.co.uk>

---
+++ web/deployment-server/server.js
@@ -0,0 +1,64 @@
+/* @flow */
+
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const ejs = require('ejs');
+
+const app = express();
+const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.options('*', cors());
+
+const REACT_APP_ENV = {
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+  REACT_APP_API_SPEC_URL: process.env.REACT_APP_API_SPEC_URL,
+  REACT_APP_TOKEN_STORAGE_KEY: process.env.REACT_APP_TOKEN_STORAGE_KEY,
+  REACT_APP_KEYCLOAK_URL: process.env.REACT_APP_KEYCLOAK_URL,
+  REACT_APP_KEYCLOAK_REALM: process.env.REACT_APP_KEYCLOAK_REALM,
+  REACT_APP_KEYCLOAK_CLIENT_ID: process.env.REACT_APP_KEYCLOAK_CLIENT_ID,
+  REACT_APP_KEYCLOAK_IDP: process.env.REACT_APP_KEYCLOAK_IDP,
+  REACT_APP_GOOGLE_MAPS_API_KEY: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  REACT_APP_BOX_CLIENT_ID: process.env.REACT_APP_BOX_CLIENT_ID,
+  REACT_APP_DROPBOX_APP_KEY: process.env.REACT_APP_DROPBOX_APP_KEY,
+  REACT_APP_GOOGLE_DRIVE_CLIENT_ID:
+    process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID,
+  REACT_APP_GOOGLE_DRIVE_DEVELOPER_KEY:
+    process.env.REACT_APP_GOOGLE_DRIVE_DEVELOPER_KEY,
+  REACT_APP_ONEDRIVE_CLIENT_ID: process.env.REACT_APP_ONEDRIVE_CLIENT_ID,
+};
+
+// static assets
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+
+// serve index.ejs template for all unmatched routes
+app.get('*', async (req, res, next) => {
+  if (req.accepts('html')) {
+    const indexTemplate = path.join(__dirname, 'index.ejs');
+    const str = await ejs.renderFile(indexTemplate, {
+      REACT_APP_ENV: JSON.stringify(REACT_APP_ENV),
+    });
+    res.send(str);
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(port, host, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(`Listening at http://${host}:${port}`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('Stopping dev server');
+  server.close(() => {
+    process.exit(0);
+  });
+});
+
+module.exports = server;
