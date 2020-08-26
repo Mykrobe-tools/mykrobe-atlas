66346ea5b9ff29104cc9ecc9be95ada4fd52f8e9
Simon Heys <si@simonheys.com>
2020-08-26 09:38:54 +0100

    Add link to privacy policy (#249)

--- app/components/ui/footer/Footer.js
+++ app/components/ui/footer/Footer.js
@@ -1,7 +1,6 @@
 /* @flow */
 
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import { Container } from 'reactstrap';
 
 import styles from './Footer.module.scss';
@@ -11,26 +10,15 @@ class Footer extends React.Component<*> {
     return (
       <Container fluid className={styles.container}>
         <div className={styles.contentWrap}>
-          <div className={styles.item}>Copyright and hygiene links</div>
           <div className={styles.item}>
-            <Link className={styles.link} to="/about">
-              About
-            </Link>
-          </div>
-          <div className={styles.item}>
-            <Link className={styles.link} to="/contact">
-              Contact
-            </Link>
-          </div>
-          <div className={styles.item}>
-            <Link className={styles.link} to="/legal">
-              Terms and Conditions
-            </Link>
-          </div>
-          <div className={styles.item}>
-            <Link className={styles.link} to="/legal">
+            <a
+              className={styles.link}
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.ebi.ac.uk/data-protection/privacy-notice/mykrobe-atlas-database"
+            >
               Privacy Policy
-            </Link>
+            </a>
           </div>
         </div>
       </Container>
