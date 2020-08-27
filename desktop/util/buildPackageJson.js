33eef1c90bd5a79a54e3871b9a14b1dd8a89ef06
Simon Heys <si@simonheys.com>
2020-08-27 12:48:28 +0100

    Set values in build package

--- desktop/util/buildPackageJson.js
+++ desktop/util/buildPackageJson.js
@@ -5,12 +5,22 @@ import fs from 'fs-extra';
 
 const pkg = require('../../package.json');
 
+import { getCurrentPredictorBinariesVersion } from './predictorBinaries';
+
 export const updateBuildPackageJson = (extra: any) => {
   const packageJson = require('../resources/package.json');
+  packageJson.name = pkg.name;
   packageJson.targetName = pkg.targetName;
   packageJson.description = pkg.description;
   packageJson.productName = pkg.productName;
   packageJson.version = pkg.version;
+  packageJson.author = pkg.author;
+  packageJson.license = pkg.license;
+
+  const executableVersion = getCurrentPredictorBinariesVersion();
+  if (executableVersion) {
+    packageJson.executableVersion = executableVersion;
+  }
 
   if (extra) {
     Object.assign(packageJson, { ...extra });
