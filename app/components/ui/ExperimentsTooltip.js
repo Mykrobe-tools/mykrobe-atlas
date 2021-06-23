cdff6917e6c0aae346d1b8405aebdac1ad896e0b
Simon Heys <si@simonheys.com>
2021-06-23 14:35:13 +0100

    Optimisations

--- app/components/ui/ExperimentsTooltip.js
+++ app/components/ui/ExperimentsTooltip.js
@@ -7,63 +7,35 @@ import ExperimentsList from './ExperimentsList';
 
 import styles from './ExperimentsTooltip.module.scss';
 
-class ExperimentsTooltip extends React.PureComponent<*> {
-  onRef = (ref) => {
-    this._ref = ref;
-  };
-
-  componentDidMount = () => {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  };
-
-  componentWillUnmount = () => {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  };
-
-  handleClickOutside = (event) => {
-    const { onClickOutside } = this.props;
-    if (!onClickOutside) {
-      return;
-    }
-    if (this._ref && !this._ref.contains(event.target)) {
-      // FIXME: this is a crude way of allowing clicking on a navigation link outside
-      // this tooltip - possibly on a different tooltip - to propagate
-      setTimeout(onClickOutside, 500);
-    }
-  };
-
-  render() {
-    const { x, y, experiment, experiments } = this.props;
-    if (!experiments || !experiments.length) {
-      return null;
-    }
-    return (
-      <div
-        className={styles.tooltip}
-        style={{ left: x, top: y }}
-        ref={this.onRef}
-      >
-        <div className={styles.tooltipWrapper}>
-          <div className={styles.tooltipContainer}>
-            <div className={styles.tooltipContent}>
-              <ExperimentsList
-                experiment={experiment}
-                experiments={experiments}
-                expandable
-                minExpandable={4}
-              />
-            </div>
+const ExperimentsTooltip = ({
+  x = 0,
+  y = 0,
+  experiment,
+  experiments = [],
+}: React.ElementProps<*>) => {
+  const style = React.useMemo(() => {
+    return { left: x, top: y };
+  }, [x, y]);
+  if (!experiments.length) {
+    return null;
+  }
+  return (
+    <div className={styles.tooltip} style={style}>
+      <div className={styles.tooltipWrapper}>
+        <div className={styles.tooltipContainer}>
+          <div className={styles.tooltipContent}>
+            <ExperimentsList
+              experiment={experiment}
+              experiments={experiments}
+              expandable
+              minExpandable={4}
+            />
           </div>
         </div>
       </div>
-    );
-  }
-
-  static defaultProps = {
-    x: 0,
-    y: 0,
-  };
-}
+    </div>
+  );
+};
 
 ExperimentsTooltip.propTypes = {
   experiments: PropTypes.array,
